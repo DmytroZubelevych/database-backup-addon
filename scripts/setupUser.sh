acfc8b1cde062e8323b126092cc6891b4d5dd9f5
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-09-29 15:09:31 +0300

    Update setupUser.sh

--- scripts/setupUser.sh
+++ scripts/setupUser.sh
@@ -2,7 +2,6 @@
 
 USER=$1
 PASSWORD=$2
-ADMIN_PASSWORD=$(pwgen 10 1)
 JEM=$(which jem)
 MYSQL=$(which mysql)
 cmd="CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASSWORD'; CREATE USER '$USER'@'%' IDENTIFIED BY '$PASSWORD'; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'localhost' WITH GRANT OPTION; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
@@ -10,7 +9,7 @@ unset resp;
 resp=$(mysql -u$USER -p$PASSWORD mysql --execute="SHOW COLUMNS FROM user")
 [ -z "$resp" ] && {
    encPass=$(echo $ADMIN_PASSWORD | openssl enc -e -a -A -aes-128-cbc -nosalt -pass "pass:TFVhBKDOSBspeSXesw8fElCcOzbJzYed")
-   $JEM passwd set -p static:$encPass
+   eval mysqld_safe --skip-grant-tables --skip-networking --skip-slave-start --user=mysql  --pid-file=$pid &
    $MYSQL -uroot -p${ADMIN_PASSWORD} --execute="$cmd"
 } || {
    echo "[Info] User $user has the required access to the database."
