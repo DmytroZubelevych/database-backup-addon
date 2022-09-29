8df270083d3dfd403e81f3b2cff7fba2942cd0a2
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-09-29 21:54:26 +0300

    Update backup-logic.sh

--- scripts/backup-logic.sh
+++ scripts/backup-logic.sh
@@ -49,6 +49,7 @@ function backup(){
             PGPASSWORD="${DBPASSWD}" pg_dumpall -U ${DBUSER} > db_backup.sql || { echo "DB backup process failed."; exit 1; }
             sed -ci -e '/^ALTER ROLE webadmin WITH SUPERUSER/d' db_backup.sql 
         else
+            mysql -h localhost -u ${DBUSER} -p${DBPASSWD} mysql --execute="SHOW COLUMNS FROM user" || { echo "DB credentials specified in add-on settings are incorrect!"; exit 1; }
             mysqldump -h localhost -u ${DBUSER} -p${DBPASSWD} --force --single-transaction --quote-names --opt --all-databases > db_backup.sql || { echo "DB backup process failed."; exit 1; }
         fi
         RESTIC_PASSWORD=${ENV_NAME} restic -q -r /opt/backup/${ENV_NAME}  backup --tag "${DUMP_NAME} ${BACKUP_ADDON_COMMIT_ID} ${BACKUP_TYPE}" ~/db_backup.sql | tee -a ${BACKUP_LOG_FILE}
