d655fff34cf9247a24a2e4b93bb2e21d43956d4c
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-09-29 22:43:00 +0300

    Update backup-logic.sh

--- scripts/backup-logic.sh
+++ scripts/backup-logic.sh
@@ -18,7 +18,7 @@ function backup(){
     [ -d /opt/backup/${ENV_NAME}  ] || mkdir -p /opt/backup/${ENV_NAME}
     RESTIC_PASSWORD=${ENV_NAME} restic -q -r /opt/backup/${ENV_NAME}  snapshots || RESTIC_PASSWORD=${ENV_NAME} restic init -r /opt/backup/${ENV_NAME} 
     echo $(date) ${ENV_NAME}  "Checking the backup repository integrity and consistency before adding the new snapshot" | tee -a ${BACKUP_LOG_FILE}
-    RESTIC_PASSWORD=${ENV_NAME} restic -q -r /opt/backup/${ENV_NAME} check | tee -a ${BACKUP_LOG_FILE} || { echo "Backup repository integrity check failed."; exit 1; }
+    RESTIC_PASSWORD=${ENV_NAME} restic -q -r /opt/backup/${ENV_NAME} check | tee -a ${BACKUP_LOG_FILE}
     source /etc/jelastic/metainf.conf;
     if [ "$COMPUTE_TYPE" == "redis" ]; then
         if grep -q '^cluster-enabled yes' /etc/redis.conf; then
@@ -39,24 +39,22 @@ function backup(){
             export MASTERS_LIST=$(redis-cli cluster nodes|grep master|grep -v fail|awk '{print $2}'|awk -F : '{print $1}');
             for i in $MASTERS_LIST
             do
-                redis-cli -h $i --rdb /tmp/redis-dump-cluster-$i.rdb || { echo "DB backup process failed."; exit 1; }
+                redis-cli -h $i --rdb /tmp/redis-dump-cluster-$i.rdb
             done
         fi
         RDB_TO_BACKUP=$(ls -d /tmp/* |grep redis-dump.*);
         RESTIC_PASSWORD=${ENV_NAME} restic -q -r /opt/backup/${ENV_NAME}  backup --tag "${DUMP_NAME} ${BACKUP_ADDON_COMMIT_ID} ${BACKUP_TYPE}" ${RDB_TO_BACKUP} | tee -a ${BACKUP_LOG_FILE};
     else
         if [ "$COMPUTE_TYPE" == "postgres" ]; then
-            PGPASSWORD="${DBPASSWD}" psql -U ${DBUSER} -d postgres -c "SELECT current_user" || { echo "DB credentials specified in add-on settings are incorrect!"; exit 1; }
-            PGPASSWORD="${DBPASSWD}" pg_dumpall -U ${DBUSER} > db_backup.sql || { echo "DB backup process failed."; exit 1; }
+            PGPASSWORD="${DBPASSWD}" pg_dumpall -U ${DBUSER} > db_backup.sql
             sed -ci -e '/^ALTER ROLE webadmin WITH SUPERUSER/d' db_backup.sql 
         else
-            mysql -h localhost -u ${DBUSER} -p${DBPASSWD} mysql --execute="SHOW COLUMNS FROM user" || { echo "DB credentials specified in add-on settings are incorrect!"; exit 1; }
-            mysqldump -h localhost -u ${DBUSER} -p${DBPASSWD} --force --single-transaction --quote-names --opt --all-databases > db_backup.sql || { echo "DB backup process failed."; exit 1; }
+            mysqldump -h localhost -u ${DBUSER} -p${DBPASSWD} --force --single-transaction --quote-names --opt --all-databases > db_backup.sql
         fi
         RESTIC_PASSWORD=${ENV_NAME} restic -q -r /opt/backup/${ENV_NAME}  backup --tag "${DUMP_NAME} ${BACKUP_ADDON_COMMIT_ID} ${BACKUP_TYPE}" ~/db_backup.sql | tee -a ${BACKUP_LOG_FILE}
     fi
     echo $(date) ${ENV_NAME} "Rotating snapshots by keeping the last ${BACKUP_COUNT}" | tee -a ${BACKUP_LOG_FILE}
-    RESTIC_PASSWORD=${ENV_NAME} restic forget -q -r /opt/backup/${ENV_NAME}  --keep-last ${BACKUP_COUNT} --prune | tee -a ${BACKUP_LOG_FILE} || { echo "Backup rotation failed."; exit 1; }
+    RESTIC_PASSWORD=${ENV_NAME} restic forget -q -r /opt/backup/${ENV_NAME}  --keep-last ${BACKUP_COUNT} --prune | tee -a ${BACKUP_LOG_FILE}
     echo $(date) ${ENV_NAME} "Checking the backup repository integrity and consistency after adding the new snapshot and rotating old ones" | tee -a ${BACKUP_LOG_FILE}
     RESTIC_PASSWORD=${ENV_NAME} restic -q -r /opt/backup/${ENV_NAME}  check --read-data-subset=1/10 | tee -a ${BACKUP_LOG_FILE}
     rm -f /var/run/${ENV_NAME}_backup.pid
