d130db02f64d5741c3ae70c79eb544132a1e2005
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-10-04 18:13:01 +0300

    Update checkCredentials.sh

--- scripts/checkCredentials.sh
+++ scripts/checkCredentials.sh
@@ -7,10 +7,8 @@ function checkCredentials(){
     source /etc/jelastic/metainf.conf 
     if [ "$COMPUTE_TYPE" == "postgres" ]; then
         PGPASSWORD="${DBPASSWD}" psql -U ${DBUSER} -d postgres -c "SELECT current_user" || exit 1;
-    elif [ "$COMPUTE_TYPE" == "mariadb" ] || [ "$COMPUTE_TYPE" == "mysql" ] || [ "$COMPUTE_TYPE" == "percona" ]; the
-n
-        mysql -h localhost -u ${DBUSER} -p${DBPASSWD} mysql --execute="SHOW COLUMNS FROM user" 1>/dev/null || exit 1
-;
+    elif [ "$COMPUTE_TYPE" == "mariadb" ] || [ "$COMPUTE_TYPE" == "mysql" ] || [ "$COMPUTE_TYPE" == "percona" ]; then
+        mysql -h localhost -u ${DBUSER} -p${DBPASSWD} mysql --execute="SHOW COLUMNS FROM user" 1>/dev/null || exit 1;
     else
         true;
     fi
