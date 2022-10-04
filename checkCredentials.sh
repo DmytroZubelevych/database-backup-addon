8ea2aa7331149c51b85fa44e3f966997d2e8fc2a
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-10-04 14:27:16 +0300

    Create checkCredentials.sh

---
+++ checkCredentials.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DBUSER=$2
+DBPASSWD=$3
+
+function checkCredentials(){
+    if [ "$COMPUTE_TYPE" == "postgres" ]; then
+        PGPASSWORD="${DBPASSWD}" psql -U ${DBUSER} -d postgres -c "SELECT current_user" || exit 1;
+    elif [ "$COMPUTE_TYPE" == "mariadb" ] || [ "$COMPUTE_TYPE" == "mysql" ] || [ "$COMPUTE_TYPE" == "percona" ]; then
+        mysql -h localhost -u ${DBUSER} -p${DBPASSWD} mysql --execute="SHOW COLUMNS FROM user" || exit 1;
+    else
+        true;
+    fi
+}
+
+if [ "x$1" == "xcheckCredentials" ]; then
+    checkCredentials
+fi
