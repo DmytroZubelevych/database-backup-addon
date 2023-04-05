c41650c4f3fb387b34ef760f7e7b1d5e3e1e9f6e
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2023-04-05 16:46:35 +0300

    Update backup-manager.js

--- scripts/backup-manager.js
+++ scripts/backup-manager.js
@@ -208,7 +208,8 @@ function BackupManager(config) {
     }
 
     me.addMountForBackupRestore = function addMountForBackupRestore() {
-        var resp = jelastic.env.file.AddMountPointById(config.envName, session, config.backupExecNode, "/opt/backup", 'nfs4', null, '/data/', config.storageNodeId, 'DBBackupRestore', false);
+	var delay = Math.floor(Math.random() * 50);
+        setTimeout(() => { var resp = jelastic.env.file.AddMountPointById(config.envName, session, config.backupExecNode, "/opt/backup", 'nfs4', null, '/data/', config.storageNodeId, 'DBBackupRestore', false)}, delay );
         if (resp.result != 0) {
             var title = "Backup storage " + config.storageEnv + " is unreacheable",
                 text = "Backup storage environment " + config.storageEnv + " is not accessible for storing backups from " + config.envName + ". The error message is " + resp.error;
