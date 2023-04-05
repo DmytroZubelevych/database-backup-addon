c10528b1974b44625790557a8d518fa0ff133416
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2023-04-05 18:36:23 +0300

    Update backup-manager.js

--- scripts/backup-manager.js
+++ scripts/backup-manager.js
@@ -208,7 +208,9 @@ function BackupManager(config) {
     }
 
     me.addMountForBackupRestore = function addMountForBackupRestore() {
-        var resp = jelastic.env.file.AddMountPointByGroup(config.envName, session, config.nodeGroup, "/opt/backup", 'nfs4', null, '/data/', config.storageNodeId, 'DBBackupRestore', false);
+	var delay = (Math.floor(Math.random() * 50) * 1000);
+	java.lang.Thread.sleep(delay);
+	var resp = jelastic.env.file.AddMountPointById(config.envName, session, config.backupExecNode, "/opt/backup", 'nfs4', null, '/data/', config.storageNodeId, 'DBBackupRestore', false);
         if (resp.result != 0) {
             var title = "Backup storage " + config.storageEnv + " is unreacheable",
                 text = "Backup storage environment " + config.storageEnv + " is not accessible for storing backups from " + config.envName + ". The error message is " + resp.error;
@@ -225,7 +227,19 @@ function BackupManager(config) {
         var allMounts = jelastic.env.file.GetMountPoints(config.envName, session, config.backupExecNode).array;
         for (var i = 0, n = allMounts.length; i < n; i++) {
             if (allMounts[i].path == "/opt/backup" && allMounts[i].type == "INTERNAL") {
+                return jelastic.env.file.RemoveMountPointById(config.envName, session, config.backupExecNode, "/opt/backup");
+                if (resp.result != 0) {
+                    return resp;
+                }
+            }
+        }
+        allMounts = jelastic.env.file.GetMountPoints(config.envName, session).array;
+        for (var i = 0, n = allMounts.length; i < n; i++) {
+            if (allMounts[i].path == "/opt/backup" && allMounts[i].type == "INTERNAL") {
                 return jelastic.env.file.RemoveMountPointByGroup(config.envName, session, config.nodeGroup, "/opt/backup");
+                if (resp.result != 0) {
+                    return resp;
+                }
             }
         }
         return {
